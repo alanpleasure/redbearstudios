f78e65c5f21ec9c398f484c99fc2fbdb39355547
alanpleasure <ngonz141@gmail.com>
2015-10-25 16:12:21 -0400

    redbear

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
     config.cache_classes = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
     config.assets.compile = true
     config.assets.digest = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,25 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.cache_classes = true
+    config.serve_static_files = true
+    config.assets.compile = true
+    config.assets.digest = true
+
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "redbearstudios.com",
+    authentication: :plain,
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USER'],
+    password: ENV['GMAIL_PASS']
+}
+
+
 end
